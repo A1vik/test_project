9ab18998c1ad54b7db61d71fbdc946edc7a72829
Aleksandr <Aleksandr_Berezin@epam.com>
2020-04-23 20:12:59 +0400

    fix

---
+++ client/build/precache-manifest.6436a430c426913f4cb493191632bc52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabe41ebc980f9c6663aac338c83e1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7d61d59b472c8ef095",
+    "url": "/static/css/main.885ad857.chunk.css"
+  },
+  {
+    "revision": "a2900ff144c7c1f53949",
+    "url": "/static/js/2.d090d3f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d090d3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7d61d59b472c8ef095",
+    "url": "/static/js/main.52c80b93.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
