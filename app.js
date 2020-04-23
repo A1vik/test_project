03e405a3bf249a7a3abc8aa2228cbe10e761d590
Aleksandr <Aleksandr_Berezin@epam.com>
2020-04-23 20:24:03 +0400

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/api', apiRoutes);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'my-app', 'build')));
+  // app.use('/', express.static(path.join(__dirname, 'client', 'my-app', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'my-app', 'build', 'index.html'));
